ccc0fc93d7cba9e87da258fb5fe951a73350d031
Xotia <gaillardk621@gmail.com>
2026-01-11 13:37:41 +0100

    1.2.1 correction affichage pokedex + message cooldown

--- src/methods/message/displayCannotCatchMessage.ts
+++ src/methods/message/displayCannotCatchMessage.ts
@@ -9,6 +9,6 @@ export function displayCannotCatchMessage(
   const remainingSeconds = Math.floor((timeLeftMs % 60000) / 1000);
 
   return interaction.editReply(
-    `⏳ Tu dois encore attendre ${remainingMinutes} minute(s) et ${remainingSeconds} seconde(s) avant de refaire /random.`,
+    `⏳ Tu dois encore attendre ${remainingMinutes} minute(s) et ${remainingSeconds} seconde(s) avant de refaire /random-capture.`,
   );
 }
