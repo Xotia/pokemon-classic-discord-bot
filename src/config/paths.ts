b1fd3091bade9c288cebae698b7c108a68142e8d
Xotia <gaillardk621@gmail.com>
2026-04-09 16:02:13 +0200

    corrections 2.0.2

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -4,4 +4,3 @@ export const DATA_DIR = path.resolve('data');
 export const POKEMON_DB = path.join(DATA_DIR, 'pokemon-list.json');
 export const PLAYERS_DB = path.join(DATA_DIR, 'players.json');
 export const STATS_DB = path.join(DATA_DIR, 'stats.json');
-export const POKEMON_GEN2_DB = path.join(DATA_DIR, 'pokemon-gen2.json')
